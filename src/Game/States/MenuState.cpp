95c23f8397be3e17001781e5c5c6d23ffba68864
CarlosxFerrer <carlos.ferrer5@upr.edu>
2023-03-22 20:07:46 -0400

    Added instructions to Main Menu

--- src/Game/States/MenuState.cpp
+++ src/Game/States/MenuState.cpp
@@ -15,6 +15,12 @@ void MenuState::tick() {
 void MenuState::render() {
 	ofSetBackgroundColor(230, 230, 250);
 	startButton->render();
+	string e = "Press 'e' to pick up ingredients when in front of the counter";
+	ofDrawBitmapString("Press 'e' to pick up ingredients when in front of the counter", ofGetWidth()/2 - e.length()*3.5, ofGetHeight()/2);
+	string s = "Press 's' to serve client";
+	ofDrawBitmapString("Press 's' to serve client", ofGetWidth()/2 - s.length()*3.5, ofGetHeight()/2 + 25);
+
+
 }
 
 void MenuState::keyPressed(int key){
