69a46e61c7a076f1397ab9ffaaef2424c6b5de24
alejandro_lopez24 <alejandro.lopez24@upr.edu>
2023-04-08 18:08:59 -0400

    fixed equals method

--- src/Game/Entities/Static/Burger.cpp
+++ src/Game/Entities/Static/Burger.cpp
@@ -34,17 +34,15 @@ void Burger::clear(){
 }
 
 bool Burger::equals(Burger* playerBurger){
-    if (playerBurger->getIngredients().size() == this->getIngredients().size()){ // if this is failed then the burgers are not the same
-        if (playerBurger->getIngredients().front()->getName() == "topBread" && this->getIngredients().front()->getName() == "topBread"
-        && playerBurger->getIngredients().back()->getName() == "botBread" && this->getIngredients().back()->getName() == "botBread"){ // checks if the first and last ingredients of the target and parameter burgers are the corresponding buns
-            std::vector<Item *> playerBurgerCopy(playerBurger->getIngredients()); // gives us vector list of parameter burger
-            std::vector<Item *> thisCopy(this->getIngredients()); // gives us vector list of target burger
-            for (int i = 1; i < thisCopy.size()-1; i++){
-                for (int j = 1; i != j; j++) { // nested for loop is used to verify if each ingredient is the same in both burgers, does not take into account the ingredient in the same position for the same burger (thats why i != j)
-                if (playerBurgerCopy[i]->getName() == thisCopy[j]->getName()) {isEqual = true;} // returns true if all ingredients are the same and in the same position as in the other burger but not itself (thats why i != j)
-                else {isEqual = false;} // if one ingredient is not the same then it return false
-                }
-            }
+    std::vector<Item *> playerBurgerCopy(playerBurger->getIngredients()); // gives us vector list of target burger
+    std::vector<Item *> thisCopy(this->getIngredients()); //gives us vector list of parameter burger
+    for (int i = 0; i < (int)playerBurgerCopy.size(); i++){
+        for (int j = 0; j < (int)thisCopy.size(); j++){ // nested for loop goes through every ingredient
+            if (playerBurger->getIngredients()[i] == this->getIngredients()[j]) { // checks if current ingredient is in both burgers, returns true if so
+                isEqual = true;
+                break;
+            } 
+            else if (playerBurger->getIngredients().size() != this->getIngredients().size()) {return false;} // if not equal size returns false, avoids various bugs in program
         }
-    } return isEqual;  // if any previous if before nested for loop is not met returns false, if nested for loop ends with isEqual being true then returns true
+    } return isEqual; // final return for function
 }
