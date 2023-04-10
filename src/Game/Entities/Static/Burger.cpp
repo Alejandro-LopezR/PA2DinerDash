c61e12a83538587e3d5271782b094139dcea5e3d
alejandro_lopez24 <alejandro.lopez24@upr.edu>
2023-04-10 16:20:12 -0400

    fixed equals method bug

--- src/Game/Entities/Static/Burger.cpp
+++ src/Game/Entities/Static/Burger.cpp
@@ -36,13 +36,13 @@ void Burger::clear(){
 bool Burger::equals(Burger* playerBurger){
     std::vector<Item *> playerBurgerCopy(playerBurger->getIngredients()); // gives us vector list of target burger
     std::vector<Item *> thisCopy(this->getIngredients()); //gives us vector list of parameter burger
-    for (int i = 0; i < (int)playerBurgerCopy.size(); i++){
-        for (int j = 0; j < (int)thisCopy.size(); j++){ // nested for loop goes through every ingredient
-            if (playerBurger->getIngredients()[i] == this->getIngredients()[j]) { // checks if current ingredient is in both burgers, returns true if so
+    for (int i = 1; i < (int)playerBurgerCopy.size()-1; i++){
+        for (int j = 1; j < (int)thisCopy.size()-1; j++){ // nested for loop goes through every ingredient
+            if (playerBurger->getIngredients().size() != this->getIngredients().size()) {return false;} // if not equal size returns false, avoids various bugs in program
+            else if (playerBurger->getIngredients()[i] == this->getIngredients()[j]) { // checks if current ingredient is in both burgers, returns true if so
                 isEqual = true;
                 break;
-            } 
-            else if (playerBurger->getIngredients().size() != this->getIngredients().size()) {return false;} // if not equal size returns false, avoids various bugs in program
+            }
         }
     } return isEqual; // final return for function
 }
